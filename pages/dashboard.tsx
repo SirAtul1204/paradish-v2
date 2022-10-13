7be2041471954f052561a99ed29f3a57bf032d53
Atul Singh <asatulsingh20@gmail.com>
2022-10-14 01:30:13 +0530

    minor fixes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Grid, Typography } from "@mui/material";
+import { Box, Button, Card, Grid, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import CardButton from "../components/CardButton";
 import MaterialImage from "../components/MaterialImage";
@@ -12,16 +12,35 @@ const Dashboard = () => {
       image: "/assets/employee.png",
       onClick: () => router.push("/employees"),
     },
+    {
+      title: "Inventory",
+      image: "/assets/inventory.png",
+      onClick: () => router.push("/inventory"),
+    },
+    {
+      title: "Menu",
+      image: "/assets/menu.png",
+      onClick: () => router.push("/menu"),
+    },
   ];
 
   return (
-    <Grid container justifyContent="center" alignItems="center" spacing={2}>
-      <Grid item>
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <Grid container justifyContent="center" alignItems="center" spacing={2}>
         {buttons.map((button) => (
-          <CardButton key={button.title} {...button} />
+          <Grid item key={button.title}>
+            <CardButton {...button} />
+          </Grid>
         ))}
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
